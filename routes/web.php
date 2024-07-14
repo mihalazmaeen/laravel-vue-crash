5a15a4f21e982006fbb557ba40b0f69cd3038be8
mihalazmaeen <azmaeenmihal1@gmail.com>
2024-07-14 12:12:16 +0600

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/example', function () {
+    return view('example');
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
